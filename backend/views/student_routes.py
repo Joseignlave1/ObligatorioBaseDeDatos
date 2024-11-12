bdc2075664573d9046b13252775c4dd3c4a913a4
Jose Ignacio Lavecchia Suarez <127800259+Joseignlave1@users.noreply.github.com>
2024-11-12 18:27:35 -0300

    Integrated Mica Changes

--- backend/views/student_routes.py
+++ backend/views/student_routes.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, jsonify, request
+<<<<<<< HEAD
 from flask_jwt_extended import jwt_required
 from ..controllers.student_controller  import (
+=======
+from backend.controllers.student_controller  import (
+>>>>>>> 3ced2683 (Refactorize main)
     getAllStudentsEndpoint,
     getStudentById,
     addStudentEndpoint,
