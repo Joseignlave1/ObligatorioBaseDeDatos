24af84f341d25bee24cc423eaff0a8ee91d2bbcb
royarispe <royarispe001@gmail.com>
2024-11-21 11:07:17 -0300

    Refactor addClass and modifyClass to use getInstructorById controller

--- backend/models/classModels.py
+++ backend/models/classModels.py
@@ -1,15 +1,5 @@
 from ..db_connection import get_db_connection
 
-def getInstructorByCi(ci_instructor):
-    connection = get_db_connection()
-    cursor = connection.cursor(dictionary=True)
-    query = "SELECT * FROM instructores WHERE ci = %s"
-    cursor.execute(query, (ci_instructor,))
-    instructor = cursor.fetchone()
-    cursor.close()
-    connection.close()
-    return instructor
-
 def getClassesWithInstructorInShift(ci_instructor, id_shift):
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
