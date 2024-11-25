0fe9892c0dca072ecbc03c92a26342b0e3c6d690
royarispe <royarispe001@gmail.com>
2024-11-25 00:20:27 -0300

    Add complete class management frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Register from "./views/Register/Register";
 import Login from './views/Login/Login';
 import ControlPanel from './views/ControlPanel/ControlPanel';
 import ManageActivities from './views/ManageActivities/ManageActivities';
-// import ManageClases from './views/ManageClasses/ManageClasses';
+import ManageClasses from "./views/ManageClasses/ManageClasses";
 // import ManageInstructors from './views/ManageInstructors/ManageInstructors';
 import ManageShifts from './views/ManageShifts/ManageShifts';
 import ManageStudents from './views/ManageStudents/ManageStudents';
@@ -15,21 +15,20 @@ import './App.css';
 const App = () => {
 
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/control-panel" element={<ControlPanel />} />
-          <Route path="/manage-activities" element={<ManageActivities />} />
-          {/* <Route path="/manage-clases" element={<ManageClases />} />
-          <Route path="/manage-instructors" element={<ManageInstructors />} />*/}
-          <Route path="/manage-shifts" element={<ManageShifts />} /> 
-          <Route path="/manage-students" element={<ManageStudents />} />
-          <Route path="/reports" element={<Reports />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/control-panel" element={<ControlPanel />} />
+        <Route path="/manage-activities" element={<ManageActivities />} />
+        <Route path="/manage-classes" element={<ManageClasses />} />
+        {/* <Route path="/manage-instructors" element={<ManageInstructors />} />*/}
+        <Route path="/manage-shifts" element={<ManageShifts />} />
+        <Route path="/manage-students" element={<ManageStudents />} />
+        <Route path="/reports" element={<Reports />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
